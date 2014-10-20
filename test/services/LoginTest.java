4d02a9b88bbc5c301a4e42710da83c0af978bdd7
emac629 <emcaleer@regis.edu>
2014-10-20 12:55:46 -0600

    Login Test updated

--- test/services/LoginTest.java
+++ test/services/LoginTest.java
@@ -1,88 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package services;
-
-import domain.Login;
+import domain.*;
 import junit.framework.TestCase;
 
-/**
- *
- * @author Erin
- */
 public class LoginTest extends TestCase {
-    
-    public LoginTest(String testName) {
-        super(testName);
-    }
-    
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-    }
-    
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
+    public void testCreate() throws ClassaroleSvcException {
+        Factory fac = new Factory();
+        ILoginSvc ils = fac.getLoginSvc();
+        Login login = new Login();
+        login.setUsername("emcaleer");
+        login.setPassword("password");
+        login.setAcceptTerms(true);
+        Login result = ils.create(login);
+        assertNotNull(result);
     }
 
-    /**
-     * Test of create method, of class LoginSvcSioImpl.
-     */
-    public void testCreate() throws Exception {
-        System.out.println("create");
-        Login login = null;
-        LoginSvcSioImpl instance = new LoginSvcSioImpl();
-        Login expResult = null;
-        Login result = instance.create(login);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testRetrieve() throws ClassaroleSvcException {
+        Factory fac = new Factory();
+        IAccountSvc ias = fac.getAccountSvc();
+        Login login = new Login();
+        login.setId(1);
+        Account result = ias.retrieve(1);
+        assertNotNull(result);
     }
 
-    /**
-     * Test of retrieve method, of class LoginSvcSioImpl.
-     */
-    public void testRetrieve() throws Exception {
-        System.out.println("retrieve");
-        int id = 0;
-        LoginSvcSioImpl instance = new LoginSvcSioImpl();
-        Login expResult = null;
-        Login result = instance.retrieve(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testUpdate() throws ClassaroleSvcException {
+        Factory fac = new Factory();
+        IAccountSvc ias = fac.getAccountSvc();
+        Account account = new Account();
+        Login login = new Login();
+        login.setUsername("wwanlwess");
+        login.setPassword("password");
+        login.setAcceptTerms(true);
+        account.setLogin(login);
+        Account result = ias.update(account);
+        assertNotNull(result);
     }
 
-    /**
-     * Test of update method, of class LoginSvcSioImpl.
-     */
-    public void testUpdate() throws Exception {
-        System.out.println("update");
-        Login login = null;
-        LoginSvcSioImpl instance = new LoginSvcSioImpl();
-        Login expResult = null;
-        Login result = instance.update(login);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testDelete() throws ClassaroleSvcException {
+        Factory fac = new Factory();
+        ILoginSvc ils = fac.getLoginSvc();
+        Login login = new Login();
+        login.setUsername("wwanlwess");
+        login.setPassword("password");
+        login.setAcceptTerms(true);
+        Login result = ils.create(login);
+        assertNotNull(result);
+        Login result2 = ils.delete(login);
+        assertNotNull(result2);
     }
-
-    /**
-     * Test of delete method, of class LoginSvcSioImpl.
-     */
-    public void testDelete() throws Exception {
-        System.out.println("delete");
-        Login login = null;
-        LoginSvcSioImpl instance = new LoginSvcSioImpl();
-        Login expResult = null;
-        Login result = instance.delete(login);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
