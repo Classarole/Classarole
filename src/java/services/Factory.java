b79fe017f3d14ff3df340d1faa188e1b6dd1bcc0
emac629 <emcaleer@regis.edu>
2014-10-20 12:55:04 -0600

    Login Test updated

--- src/java/services/Factory.java
+++ src/java/services/Factory.java
@@ -4,4 +4,10 @@ public class Factory {
     public IAccountSvc getAccountSvc() {
         return new AccountSvcSioImpl();
     }
+    public ILoginSvc getLoginSvc() {
+        return new LoginSvcSioImpl();
+    }
+    public IReviewSvc getReviewSvc() {
+        return new ReviewSvcSioImpl();
+    }
 }
